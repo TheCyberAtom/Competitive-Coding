9dab1c98136035ee72278282a365ae5ce607a81e
TheCyberAtom <rkm.rahulkmishra@gmail.com>
2020-10-03 19:05:18 +0530

    code solution

---
+++ CodeForces/110A_NearlyLuckyNumber.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+#define UINT unsigned int
+#define LLI long long int
+#define LL long long
+#define T testcases
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+#define PB push_back
+#define OJ \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define MOD 1000000007
+
+int main()
+{
+//OJ;
+FASTIO;
+    long long n;
+    cin >> n;
+    int count = 0;
+    while (n != 0)
+    {
+        if (n % 10 == 4 || n % 10 == 7)
+        {
+            count += 1;
+        }
+        n /= 10;
+    }
+    if (count == 4 || count == 7)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+return 0;
+}
